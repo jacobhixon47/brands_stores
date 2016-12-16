67c1660c89167a87cd8eca414981edf387c7308d
Jacob Hixon <jacobhixon47@gmail.com>
2016-12-16 08:28:23 -0800

    display up to 8 items in the 'brands' and 'stores' side bars

--- spec/brands_integration_spec.rb
+++ spec/brands_integration_spec.rb
@@ -22,3 +22,16 @@ describe('shows all brands on side menu', {:type => :feature}) do
     expect(page).to have_content('Nike')
   end
 end
+
+# describe('add brand to store', {:type => :feature}) do
+#   it('allows a user to add a brand to a store') do
+#     visit('/')
+#     fill_in('brand_name', :with => 'Nike')
+#     find('#add_brand_submit').click
+#     click_link('Home')
+#     click_link('Brands')
+#     click_link('Nike')
+#     expect(page).to have_content('Adidas')
+#     expect(page).to have_content('Nike')
+#   end
+# end
