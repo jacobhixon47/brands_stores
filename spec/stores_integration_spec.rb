5c08cee1bf4c62712fcd2fd2acd7f83c85c6f41e
Jacob Hixon <jacobhixon47@gmail.com>
2016-12-16 00:53:21 -0800

    pass integration spec for displaying all STORES in side menu list

--- spec/stores_integration_spec.rb
+++ spec/stores_integration_spec.rb
@@ -8,3 +8,17 @@ describe('add a store', {:type => :feature}) do
     expect(page).to have_content('DSW')
   end
 end
+
+describe('shows all stores on side menu', {:type => :feature}) do
+  it('allows a user to view all stores in the database') do
+    visit('/')
+    fill_in('store_name', :with => 'Saks Fifth')
+    find('#add_store_submit').click
+    click_link("Home")
+    fill_in('store_name', :with => 'DSW')
+    find('#add_store_submit').click
+    click_link("Stores")
+    expect(page).to have_content('Saks Fifth')
+    expect(page).to have_content('DSW')
+  end
+end
